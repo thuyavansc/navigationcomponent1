81d6fd8c5eed5cdcd7684386fdf9cb820fbeadfc
Kingston <Kingston@DESKTOP-MVP19NT>
2023-11-07 18:43:22 +0530

    Working: 1 Main Activity 4 Fragments

---
+++ app/src/main/java/au/com/softclient/navigationcomponent1/MainActivity.java
@@ -0,0 +1,34 @@
+package au.com.softclient.navigationcomponent1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        //Find the Bottom Navigation View
+//        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+//        //Find the Nav Controller
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_container);
+//
+//        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        // Initialize the NavController.
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_container);
+
+        // Associate the NavController with the Activity.
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}
